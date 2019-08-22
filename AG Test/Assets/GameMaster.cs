fc5ce576c99a371becf8f6dde65fb6286c2462c3
SheenTech <52928138+SheenTech@users.noreply.github.com>
2019-08-22 07:43:55 -0700

    New Player Sprite
    Added the main player sprite, does not have animation yet.

---
+++ AG Test/Assets/GameMaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster gm;
+
+    void start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag ("GM").GetComponent<GameMaster>();
+        }
+    }
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+
+    public void RespawnPlayer ()
+    {
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    public static void KillPlayer (PlayerMovement player)
+    {
+        Destroy(player.gameObject);
+        gm.RespawnPlayer();
+    }
+}
